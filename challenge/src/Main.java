0c13c02baebe7eb70ad84e737388c7a7fffc4ea3
Christian Buckowitz <christian.buckowitz@qsc.de>
2019-03-15 18:30:27 +0100

    map class

--- challenge/src/Main.java
+++ challenge/src/Main.java
@@ -3,6 +3,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+
+
+
+    // einlesen der Datei
+
+    // konvertieren der Map in Terrain-Cost-Matrix
+
 }
 
 // Dennis was here
